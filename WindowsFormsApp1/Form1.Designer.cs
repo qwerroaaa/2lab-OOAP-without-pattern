fd738870542ee2ab39dcd1956c8369433540a4e3
serbu <serbu@DESKTOP-1FP6OKN>
2024-03-26 18:48:01 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.ChoseElem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.choosePizza = new System.Windows.Forms.ComboBox();
+            this.addToPizza = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(37, 213);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 35);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Выбрать";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(237, 49);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.ReadOnly = true;
+            this.priceBox.Size = new System.Drawing.Size(83, 20);
+            this.priceBox.TabIndex = 1;
+            // 
+            // ChoseElem
+            // 
+            this.ChoseElem.Location = new System.Drawing.Point(391, 49);
+            this.ChoseElem.Multiline = true;
+            this.ChoseElem.Name = "ChoseElem";
+            this.ChoseElem.ReadOnly = true;
+            this.ChoseElem.Size = new System.Drawing.Size(140, 120);
+            this.ChoseElem.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Цена:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(388, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выбрано:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пиццы:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Добавки:";
+            // 
+            // choosePizza
+            // 
+            this.choosePizza.FormattingEnabled = true;
+            this.choosePizza.Location = new System.Drawing.Point(37, 48);
+            this.choosePizza.Name = "choosePizza";
+            this.choosePizza.Size = new System.Drawing.Size(90, 21);
+            this.choosePizza.TabIndex = 7;
+            this.choosePizza.SelectedIndexChanged += new System.EventHandler(this.choosePizza_SelectedIndexChanged);
+            // 
+            // addToPizza
+            // 
+            this.addToPizza.FormattingEnabled = true;
+            this.addToPizza.Location = new System.Drawing.Point(37, 160);
+            this.addToPizza.Name = "addToPizza";
+            this.addToPizza.Size = new System.Drawing.Size(86, 21);
+            this.addToPizza.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addToPizza);
+            this.Controls.Add(this.choosePizza);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChoseElem);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox ChoseElem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox choosePizza;
+        private System.Windows.Forms.ComboBox addToPizza;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private PizzaBasic currentPizza = new CheesePizza();
+
+        public Form1()
+        {
+            InitializeComponent();
+            choosePizza.Items.Add("Сырная пицца");
+            choosePizza.Items.Add("Барбекю пицца");
+
+            addToPizza.Items.Add("Томат");
+            addToPizza.Items.Add("Сыр");
+            addToPizza.Items.Add("Мясо");
+        }
+
+        private void UpdateForm()
+        {
+            ChoseElem.Text = currentPizza.GetDescription();
+            priceBox.Text = $"{currentPizza.GetCost()} руб.";
+        }
+
+        // Абстрактный класс базовой пиццы
+        public abstract class PizzaBasic
+        {
+            public abstract int GetCost();
+            public abstract string GetDescription();
+        }
+
+        // Класс сырной пиццы
+        public class CheesePizza : PizzaBasic
+        {
+            public override int GetCost()
+            {
+                // Цена сырной пиццы
+                return 450;
+            }
+
+            public override string GetDescription()
+            {
+                return "Сырная пицца";
+            }
+        }
+
+        // Класс барбекю пиццы
+        public class BarbecuePizza : PizzaBasic
+        {
+            public override int GetCost()
+            {
+                // Цена барбекю пиццы
+                return 560;
+            }
+
+            public override string GetDescription()
+            {
+                return "Барбекю пицца";
+            }
+        }
+
+        // Класс добавки томата
+        public class Tomato : PizzaBasic
+        {
+            private PizzaBasic pizza;
+
+            public Tomato(PizzaBasic pizza)
+            {
+                this.pizza = pizza;
+            }
+
+            public override int GetCost()
+            {
+                // Дополнительная цена за томаты
+                return pizza.GetCost() + 15;
+            }
+
+            public override string GetDescription()
+            {
+                return pizza.GetDescription() + ", Добавка: Томат";
+            }
+        }
+
+        // Класс добавки сыра
+        public class Cheese : PizzaBasic
+        {
+            private PizzaBasic pizza;
+
+            public Cheese(PizzaBasic pizza)
+            {
+                this.pizza = pizza;
+            }
+
+            public override int GetCost()
+            {
+                // Дополнительная цена за сыр
+                return pizza.GetCost() + 30;
+            }
+
+            public override string GetDescription()
+            {
+                return pizza.GetDescription() + ", Добавка: Сыр";
+            }
+        }
+
+        // Класс добавки мяса
+        public class Meat : PizzaBasic
+        {
+            private PizzaBasic pizza;
+
+            public Meat(PizzaBasic pizza)
+            {
+                this.pizza = pizza;
+            }
+
+            public override int GetCost()
+            {
+                // Дополнительная цена за мясо
+                return pizza.GetCost() + 50;
+            }
+
+            public override string GetDescription()
+            {
+                return pizza.GetDescription() + ", Добавка: Мясо";
+            }
+        }
+
+        private void choosePizza_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (choosePizza.Text)
+            {
+                case "Сырная пицца":
+                    currentPizza = new CheesePizza();
+                    break;
+                case "Барбекю пицца":
+                    currentPizza = new BarbecuePizza();
+                    break;
+                default:
+                    currentPizza = null; // Если пицца не выбрана, устанавливаем выбранный объект в null
+                    break;
+            }
+            priceBox.Text = $"{currentPizza.GetCost()} руб.";
+            UpdateForm();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            switch (addToPizza.Text)
+            {
+                case "Томат":
+                    currentPizza = new Tomato(currentPizza);
+                    break;
+                case "Сыр":
+                    currentPizza = new Cheese(currentPizza);
+                    break;
+                case "Мясо":
+                    currentPizza = new Meat(currentPizza);
+                    break;
+                default:
+                    currentPizza = null; // Если добавка не выбрана, устанавливаем выбранный объект в null
+                    break;
+            }
+            UpdateForm();
+        }
+    }
+}
